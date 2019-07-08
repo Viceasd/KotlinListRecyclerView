ae29998a59c8464c4c65203e9dee457559ddcccf
Jorge <jevicencio@gmail.com>
2019-07-08 17:38:00 -0400

    se actualiza gradle

--- app/src/main/java/com/androidmads/kotlinlistrecyclerview/MainActivity.kt
+++ app/src/main/java/com/androidmads/kotlinlistrecyclerview/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ListView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,11 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById(R.id.btnListView).setOnClickListener {
+        findViewById<View>(R.id.btnListView).setOnClickListener {
             startActivity(Intent(applicationContext, ListViewActivity::class.java))
         }
 
-        findViewById(R.id.btnRecView).setOnClickListener {
+        findViewById<Button>(R.id.btnRecView).setOnClickListener {
             startActivity(Intent(applicationContext, RecyclerViewActivity::class.java))
         }
     }
